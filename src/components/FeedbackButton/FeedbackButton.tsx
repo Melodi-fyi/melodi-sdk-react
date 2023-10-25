aaad1df0a89f99c5021f6bbb73a2ae17fb66bb6d
Gregory Brown <brown.gregm@gmail.com>
2023-10-25 16:58:58 -0700

    this should not be called Button

--- src/components/FeedbackButton/FeedbackButton.tsx
+++ src/components/FeedbackButton/FeedbackButton.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { ButtonProps } from "./FeedbackButton.types";
 
-export function Button(props: ButtonProps) {
+export function FeedbackButton(props: ButtonProps) {
   return (
     <button disabled={props.disabled} onClick={props.onClick}>
       {props.text || "Feedback"}
