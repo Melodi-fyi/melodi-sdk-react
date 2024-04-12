f76643c4ca11eced1b15719fc5ffb7daf606c8e8
Gregory Brown <brown.gregm@gmail.com>
2024-04-11 17:22:29 -0700

    pass object for new external feedback api

--- src/actions/feedback.types.ts
+++ src/actions/feedback.types.ts
@@ -1,7 +1,20 @@
-export type Feedback = {
-  feedbackType: "POSITIVE" | "NEGATIVE" | "SKIP";
-  feedbackText?: string;
+export type User = {
   username: string;
-  experimentId: number;
-  sampleId: number;
+};
+
+export type SampleCreateRequest = {
+  project: string;
+  projectVersion: string;
+  message?: string;
+  response: string;
+  title: string;
+};
+
+export type FeedbackCreateRequest = {
+  user: User;
+  sample: SampleCreateRequest;
+  feedback: {
+    feedbackType: "POSITIVE" | "NEGATIVE";
+    feedbackText?: string;
+  };
 };
