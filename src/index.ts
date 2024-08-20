b01d6d684d009dd7ac9c4f4d020a74481fb463ba
Gregory Brown <brown.gregm@gmail.com>
2024-08-20 12:22:58 -0700

    export the associated lod id types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 import './global.css';
 export { MelodiAuthProvider, useMelodiAuthContext } from './auth/MelodiAuthProvider';
 export { MelodiFeedbackWidget } from './components/feedback/MelodiFeedbackWidget';
+export {
+  AssociatedLogIds,
+  AssociatedLogOrIds,
+} from './components/feedback/popover/FeedbackPopover.types';
